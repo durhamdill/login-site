6800e1ae76eb8640e51658f1c12802686ea50d46
Katy Dillard <durhamdill@gmail.com>
2017-08-22 21:56:51 -0400

    revisit login verification

--- app.js
+++ app.js
@@ -11,99 +11,53 @@ const app = express();
 app.engine('mustache', mustache());
 app.set('views', './views');
 app.set('view engine', 'mustache');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(session({
  secret: 'dog person',
  resave: false,
- saveUninitalized: true
+ saveUninitalized: true,
+ cookie: {maxAge: 60000}
 }));
 
+app.get('/', function(req, res, next){
+    res.render('userpage');
+});
+
 app.get('/login', function(req, res){
- res.render('login');
-});
-
-app.get('/userpage', function(req, res){
-  res.render('userpage', {username: username});
-});
-
-// TRYING TO PUSH NEW USERS TO DATA FILE (NOT WORKING):
-// app.post('/login', function(req, res){
-//  let newName = req.body.username;
-//  let newPassword = req.body.password;
-//  // let newUser = {username: newName};
-//  // console.log(newUser);
-//  data.users.push({
-//    username: newName,
-//     password: newPassword});
-//  });
-
- function authenticate(req, username, password){
-  var authenticatedUser = data.users.find(function (user) {
-    if (username === user.username && password === user.password) {
-      req.session.authenticated = true;
-      console.log('User & Password Authenticated');
-    } else {
-      return false
-    }
+    res.render('login');
   });
-  console.log(req.session);
-  return req.session;
-}
 
-app.post('/userpage', function(req, res){
-  var username = req.body.username;
-  var password = req.body.password;
-  authenticate(req, username, password);
-  if (req.session && req.session.authenticated){
-    res.redirect('/userpage');
-  } else {
-    res.redirect('/login');
-  }
-})
+  app.post('/login', function(req, res){
+    // let username = req.body.username;
+    // let password = req.body.password;
+    data.users.find(function(user) {
+      if (req.body.username===user.username && req.body.password===user.password) {
+        console.log("login confirmed");
+        console.log('user');
+        res.redirect('/');
+      } else {
+          console.log("login denied");
+          res.redirect('/login');
+          }
+        })
+    });
 
- app.listen(port, function(req, res){
-  console.log('Starting top secret login app...');
- });
-
-// for (let i = 0; i < data.users.length; i++) {
-//    if (username === data.users[i].username && password === data.users[i].password) {
-//      console.log('hell yeah');
-//      req.session.authenticated = true;
-//      res.redirect('/userpage')
-//   } else if (username !== data.users[i].username || password !== data.users[i].password) {
-//      res.redirect('/login');
+// app.post('/login', function(req, res){
+//   let username = req.body.username;
+//   let password = req.body.password;
+//   if (username && password) {
+//     console.log("password and username logged!");
+//     console.log("username: " + username + " password: " + password);
+//     res.redirect('/');
+//   } else {
+//     console.log("missing username or password");
+//     console.log("username: " + username + " password: " + password);
 //   }
+// });
 
- // if (username && password) {
- //  console.log('logged in!');
- // } else {
- //  console.log('no dice!');
- // }
-// };
-
-
-
-// amys answers
-
-// this goes in post:
-//  authenticate(req, username, password);
-//  if (req.session && req.session.authenticated){
-//   res.render('login', {username: password})
-// } else {
-//   res.redirect('/');
-// }
-
-
-//this is the auth function
-// function authenticate(req, username, password){
-//   var authenticatedUser = data.users.find(function (user){
-//     if (username === user.username && password === user.password) {
-//       req.session.authenticated = true;
-//       console.log('User & Password Authenticated');
-//     } else {
-//       return false;
-//     }
-//   })
-// }
+app.listen(port, function(req, res){
+   console.log('Starting top secret login app...');
+  });
---
+++ app2.js
@@ -0,0 +1,120 @@
+const data = require('./users.js');
+const express = require('express');
+// const parseurl = require('parseurl'); //what is this for? did not install at this point
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const mustache = require('mustache-express');
+const port = 3000;
+
+const app = express();
+
+app.engine('mustache', mustache());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(session({
+ secret: 'dog person',
+ resave: false,
+ saveUninitalized: true,
+ cookie: {maxAge: 60000}
+}));
+
+app.get('/', function(req, res, next){
+  // if (req.session.views) {
+  //   req.session.views++
+  //   res.render('userpage')
+  //   res.end()
+  // } else {
+    res.redirect('/login');
+});
+
+app.get('/login', function(req, res){
+    res.render('login');
+  });
+  // res.render('userpage', {username: username});
+
+ function authenticate(req, username, password){
+  var authenticatedUser = data.users.find(function (user) {
+    if (username === user.username && password === user.password) {
+      req.session.authenticated = true;
+      console.log('User & Password Authenticated');
+      console.log("Submitted info: " + username + " & " + password);
+      console.log(user);
+    } else {
+      return false
+    }
+  });
+  // console.log(user);
+  console.log(req.session);
+  return req.session;
+}
+
+app.post('/login', function(req, res){
+  var username = req.body.username;
+  var password = req.body.password;
+  authenticate(req, username, password);
+  if (req.session && req.session.authenticated){
+    res.redirect('/');
+  } else {
+    res.redirect('/login');
+  }
+})
+
+ app.listen(port, function(req, res){
+  console.log('Starting top secret login app...');
+ });
+
+ // TRYING TO PUSH NEW USERS TO DATA FILE (NOT WORKING):
+ // app.post('/login', function(req, res){
+ //  let newName = req.body.username;
+ //  let newPassword = req.body.password;
+ //  // let newUser = {username: newName};
+ //  // console.log(newUser);
+ //  data.users.push({
+ //    username: newName,
+ //     password: newPassword});
+ //  });
+
+// for (let i = 0; i < data.users.length; i++) {
+//    if (username === data.users[i].username && password === data.users[i].password) {
+//      console.log('hell yeah');
+//      req.session.authenticated = true;
+//      res.redirect('/userpage')
+//   } else if (username !== data.users[i].username || password !== data.users[i].password) {
+//      res.redirect('/login');
+//   }
+
+ // if (username && password) {
+ //  console.log('logged in!');
+ // } else {
+ //  console.log('no dice!');
+ // }
+// };
+
+
+
+// amys answers
+
+// this goes in post:
+//  authenticate(req, username, password);
+//  if (req.session && req.session.authenticated){
+//   res.render('login', {username: password})
+// } else {
+//   res.redirect('/');
+// }
+
+
+//this is the auth function
+// function authenticate(req, username, password){
+//   var authenticatedUser = data.users.find(function (user){
+//     if (username === user.username && password === user.password) {
+//       req.session.authenticated = true;
+//       console.log('User & Password Authenticated');
+//     } else {
+//       return false;
+//     }
+//   })
+// }
